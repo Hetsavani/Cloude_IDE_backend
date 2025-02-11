1f3f5d198b5a3ce6d74c85f418482fa57bd29fe2
Het Savani <114601993+Hetsavani@users.noreply.github.com>
2025-02-11 08:28:24 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,11 @@
+# #!/bin/bash
+
+# # Update package list to ensure we install the latest versions
+# apt-get update
+
+# # Install required compilers
+# apt-get install -y default-jdk   # Java Development Kit (JDK)
+# apt-get install -y gcc           # GNU C Compiler (GCC)
+# apt-get install -y g++           # GNU C++ Compiler (G++)
 #!/bin/bash
-
-# Update package list to ensure we install the latest versions
-apt-get update
-
-# Install required compilers
-apt-get install -y default-jdk   # Java Development Kit (JDK)
-apt-get install -y gcc           # GNU C Compiler (GCC)
-apt-get install -y g++           # GNU C++ Compiler (G++)
+echo "All dependencies are installed inside the Docker container!"
