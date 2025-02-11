509c0727211b763b8c543d23b78e9c9c2472aeb2
Het Savani <114601993+Hetsavani@users.noreply.github.com>
2025-02-11 08:16:39 +0530

    Add files via upload

---
+++ render-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Update package list to ensure we install the latest versions
+apt-get update
+
+# Install required compilers
+apt-get install -y default-jdk   # Java Development Kit (JDK)
+apt-get install -y gcc           # GNU C Compiler (GCC)
+apt-get install -y g++           # GNU C++ Compiler (G++)
